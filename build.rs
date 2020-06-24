0b7039345415f02d5c042b762fabdf9775ac1c3e
Jakub Konka <kubkon@jakubkonka.com>
2020-06-24 14:22:58 +0200

    'Add first working draft of test harness;

--- build.rs
+++ build.rs
@@ -1,25 +1,22 @@
 fn main() {
     #[cfg(feature = "integration-tests")]
-    integration_tests::build_and_generate_tests()
+    integration_tests::build_packages()
 }
 
 #[cfg(feature = "integration-tests")]
 mod integration_tests {
     use anyhow::{anyhow, Result};
-    use std::collections::HashMap;
-    use std::env;
-    use std::fs::{self, read_dir, File};
-    use std::io::{self, Cursor, Write};
+    use std::io::{Cursor, Write};
     use std::path::{Path, PathBuf};
     use std::process::{Command, Stdio};
+    use std::{env, fs};
     use zip::{write::FileOptions, CompressionMethod, ZipWriter};
 
-    pub(super) fn build_and_generate_tests() {
+    pub(super) fn build_packages() {
         let out_dir = PathBuf::from(
             env::var("OUT_DIR").expect("The OUT_DIR environment variable must be set"),
         );
 
-        let mut testcases = HashMap::new();
         let packages = fs::read_dir("integration-tests").unwrap();
         for pkg in packages {
             let pkg_path = pkg.expect("valid package path").path();
@@ -27,17 +24,11 @@ mod integration_tests {
                 "cargo:rerun-if-changed={}",
                 pkg_path.join("Cargo.toml").display()
             );
-            let (testcase_name, zip_path) =
-                build_package(&pkg_path, &out_dir).expect("building package");
-            testcases.insert(testcase_name, zip_path);
+            build_package(&pkg_path, &out_dir).expect("building package");
         }
-
-        let mut out = File::create(out_dir.join("integration_tests.rs"))
-            .expect("error generating test source file");
-        generate_tests(&mut out, testcases).expect("generating tests");
     }
 
-    fn build_package(pkg_path: &Path, out_dir: &Path) -> Result<(String, PathBuf)> {
+    fn build_package(pkg_path: &Path, out_dir: &Path) -> Result<()> {
         let mut cmd = Command::new("cargo");
         cmd.args(&[
             "build",
@@ -81,31 +72,6 @@ mod integration_tests {
         let w = zip.finish()?;
         fs::write(out_dir.join(format!("{}.zip", pkg_name)), w.into_inner())?;
 
-        Ok((
-            pkg_name.to_owned(),
-            out_dir.join(format!("{}.zip", pkg_name)),
-        ))
-    }
-
-    fn generate_tests(out: &mut File, cases: HashMap<String, PathBuf>) -> Result<()> {
-        writeln!(out, "mod integration_tests {{")?;
-
-        for (case_name, zip_path) in cases {
-            writeln!(out, "    #[test]")?;
-            writeln!(out, "    fn {}() -> anyhow::Result<()> {{", case_name.replace("-", "_"))?;
-            writeln!(out, "        ExeUnitMain::deploy()?;")?;
-            writeln!(out, "        ExeUnitMain::start()?;")?;
-
-            add_test_logic(out, case_name)?;
-
-            writeln!(out, "    }}")?;
-        }
-
-        writeln!(out, "}}")?;
         Ok(())
-    }
-
-    fn add_test_logic(out: &mut File, case_name: &str) -> Result<()> {
-
     }
 }
--- tests/integration/main.rs
+++ tests/integration/main.rs
@@ -1,7 +1,12 @@
 #![cfg(feature = "integration-tests")]
 
-use std::env;
+use anyhow::Result;
+use std::mem::ManuallyDrop;
+use std::path::{Path, PathBuf};
 use std::sync::Once;
+use std::{env, fs};
+use tempfile::tempdir;
+use ya_runtime_wasi::ExeUnitMain;
 
 static LOG_INIT: Once = Once::new();
 
@@ -13,4 +18,48 @@ fn log_setup() {
     })
 }
 
-include!(concat!(env!("OUT_DIR"), "/integration_tests.rs"));
+fn get_zip_path(case_name: &str) -> PathBuf {
+    let zip_dir = env::var("OUT_DIR").expect("OUT_DIR must be set!");
+    Path::new(&zip_dir).join(format!("{}.zip", case_name))
+}
+
+#[test]
+fn rust_wasi_tutorial() -> Result<()> {
+    let workspace = ManuallyDrop::new(tempdir()?);
+    let zip_path = get_zip_path("rust-wasi-tutorial");
+    let task_pkg = workspace.path().join("rust-wasi-tutorial.zip");
+    fs::copy(zip_path, &task_pkg)?;
+
+    ExeUnitMain::deploy(workspace.path(), &task_pkg)?;
+    ExeUnitMain::start(workspace.path())?;
+
+    let contents = "This is it!";
+    let input_file_name = "input/in".to_owned();
+    let output_file_name = "output/out".to_owned();
+    let input_path = workspace.path().join(&input_file_name);
+    let output_path = workspace.path().join(&output_file_name);
+    fs::write(input_path, contents)?;
+
+    ExeUnitMain::run(
+        workspace.path(),
+        "rust-wasi-tutorial",
+        vec![
+            ["/", &input_file_name].join(""),
+            ["/", &output_file_name].join(""),
+        ],
+    )?;
+    println!("workspace = {}", workspace.path().display());
+
+    assert!(output_path.exists(), "expected 'out' file to be created");
+
+    let given = fs::read_to_string(output_path)?;
+
+    assert_eq!(
+        contents, given,
+        "'in' and 'out' should have matching contents"
+    );
+
+    // We drop manually so that in case of an error we can browse the temp dir
+    let _ = ManuallyDrop::into_inner(workspace);
+    Ok(())
+}
