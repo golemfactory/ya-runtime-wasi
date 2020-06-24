47f37ba6323f0f378931df4a9fc2303e00a75e95
Jakub Konka <kubkon@jakubkonka.com>
2020-06-24 17:18:10 +0200

    Use a simple struct for orchestrating integration tests

--- tests/integration/main.rs
+++ tests/integration/main.rs
@@ -1,65 +1,94 @@
 #![cfg(feature = "integration-tests")]
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use std::mem::ManuallyDrop;
 use std::path::{Path, PathBuf};
 use std::sync::Once;
 use std::{env, fs};
-use tempfile::tempdir;
+use tempfile::{tempdir, TempDir};
 use ya_runtime_wasi::ExeUnitMain;
 
 static LOG_INIT: Once = Once::new();
 
 fn log_setup() {
     LOG_INIT.call_once(|| {
-        let our_rust_log = "cranelift_wasm=warn,cranelift_codegen=info,wasi_common=trace";
-        env::set_var("RUST_LOG", our_rust_log);
         env_logger::init();
     })
 }
 
-fn get_zip_path(case_name: &str) -> PathBuf {
-    let zip_dir = env::var("OUT_DIR").expect("OUT_DIR must be set!");
-    Path::new(&zip_dir).join(format!("{}.zip", case_name))
+struct TestCase {
+    name: String,
+    workspace: ManuallyDrop<TempDir>,
+}
+
+impl TestCase {
+    fn new<S: AsRef<str>>(name: S) -> Self {
+        log_setup();
+
+        let workspace = ManuallyDrop::new(tempdir().expect("could create a temp dir"));
+
+        Self {
+            name: name.as_ref().to_owned(),
+            workspace,
+        }
+    }
+
+    fn with(self, logic: impl FnOnce(&Path) -> Result<()>) -> Result<()> {
+        let zip_path = self.get_zip_path();
+        let task_pkg = self.workspace.path().join(self.name);
+        fs::copy(zip_path, &task_pkg)?;
+
+        ExeUnitMain::deploy(self.workspace.path(), &task_pkg)?;
+        ExeUnitMain::start(self.workspace.path())?;
+
+        let workspace_path = self.workspace.path();
+        match logic(workspace_path)
+            .with_context(|| format!("while testing package at '{}'", workspace_path.display()))
+        {
+            Ok(()) => {
+                // All assertions passed and there was no error,
+                // safe to drop the workspace dir now!
+                let _ = ManuallyDrop::into_inner(self.workspace);
+                Ok(())
+            }
+            Err(err) => Err(err),
+        }
+    }
+
+    fn get_zip_path(&self) -> PathBuf {
+        let zip_dir = env::var("OUT_DIR").expect("OUT_DIR must be set!");
+        Path::new(&zip_dir).join(format!("{}.zip", self.name))
+    }
 }
 
 #[test]
 fn rust_wasi_tutorial() -> Result<()> {
-    let workspace = ManuallyDrop::new(tempdir()?);
-    let zip_path = get_zip_path("rust-wasi-tutorial");
-    let task_pkg = workspace.path().join("rust-wasi-tutorial.zip");
-    fs::copy(zip_path, &task_pkg)?;
+    TestCase::new("rust-wasi-tutorial").with(|workspace: &Path| {
+        let contents = "This is it!";
+        let input_file_name = "input/in".to_owned();
+        let output_file_name = "output/out".to_owned();
+        let input_path = workspace.join(&input_file_name);
+        let output_path = workspace.join(&output_file_name);
+        fs::write(input_path, contents)?;
 
-    ExeUnitMain::deploy(workspace.path(), &task_pkg)?;
-    ExeUnitMain::start(workspace.path())?;
+        ExeUnitMain::run(
+            workspace,
+            "rust-wasi-tutorial",
+            vec![
+                ["/", &input_file_name].join(""),
+                ["/", &output_file_name].join(""),
+            ],
+        )?;
 
-    let contents = "This is it!";
-    let input_file_name = "input/in".to_owned();
-    let output_file_name = "output/out".to_owned();
-    let input_path = workspace.path().join(&input_file_name);
-    let output_path = workspace.path().join(&output_file_name);
-    fs::write(input_path, contents)?;
+        assert!(output_path.exists(), "expected 'out' file to be created");
 
-    ExeUnitMain::run(
-        workspace.path(),
-        "rust-wasi-tutorial",
-        vec![
-            ["/", &input_file_name].join(""),
-            ["/", &output_file_name].join(""),
-        ],
-    )?;
-    println!("workspace = {}", workspace.path().display());
+        let given = fs::read_to_string(output_path)?;
 
-    assert!(output_path.exists(), "expected 'out' file to be created");
+        assert_eq!(
+            contents, given,
+            "'in' and 'out' should have matching contents"
+        );
 
-    let given = fs::read_to_string(output_path)?;
-
-    assert_eq!(
-        contents, given,
-        "'in' and 'out' should have matching contents"
-    );
-
-    // We drop manually so that in case of an error we can browse the temp dir
-    let _ = ManuallyDrop::into_inner(workspace);
-    Ok(())
+        Ok(())
+    })
 }
